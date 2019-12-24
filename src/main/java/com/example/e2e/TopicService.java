35c4df7b9147000591e007c0ce1896d4200a2e16
venuGopal23456 <venuvg2010@gmail.com>
2019-12-24 16:08:11 -0700

    Update Resource

--- src/main/java/com/example/e2e/TopicService.java
+++ src/main/java/com/example/e2e/TopicService.java
@@ -9,28 +9,35 @@ import java.util.List;
 @Service
 public class TopicService {
 
-  private List<topicpojo> topics =new ArrayList<>(Arrays.asList(
-            new topicpojo("venu", 25, 100),
-        new topicpojo("gopal", 67, 250),
-        new topicpojo("sharma", 87, 90),
-        new topicpojo("mani", 56, 96),
-        new topicpojo("tilo", 56, 26)));
+  private List<topicpojo> topics =
+      new ArrayList<>(
+          Arrays.asList(
+              new topicpojo("venu", 25, 100),
+              new topicpojo("gopal", 67, 250),
+              new topicpojo("sharma", 87, 90),
+              new topicpojo("mani", 56, 96),
+              new topicpojo("tilo", 56, 26)));
 
+  public List<topicpojo> getTopics() {
+    return topics;
+  }
 
-   public  List<topicpojo> getTopics()
-   {
-       return topics;
+  /// Return only specific object based on name
+  public topicpojo getTopics(String name) {
+    return topics.stream().filter(t -> t.getName().equals(name)).findFirst().get();
+  }
 
-   }
+  public void addtopic(topicpojo topic) {
+    topics.add(topic);
+  }
 
-   ///Return only specific object based on name
-   public  topicpojo getTopics(String name)
-   {
-       return topics.stream().filter(t -> t.getName().equals(name)).findFirst().get();
-}
-
-
-    public void addtopic(topicpojo topic) {
-       topics.add(topic);
+  public void updatetopic(topicpojo topic, String name) {
+    for (int i = 0; i < topics.size(); i++) {
+      topicpojo t = topics.get(i);
+      if (t.getName().equals(name)) {
+        topics.set(i, topic);
+        return;
+      }
     }
+  }
 }
--- src/main/java/com/example/e2e/topics.java
+++ src/main/java/com/example/e2e/topics.java
@@ -3,7 +3,6 @@ package com.example.e2e;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 /*This is the simple controller class
@@ -12,27 +11,30 @@ import java.util.List;
 @RestController
 public class topics {
 
-  @Autowired
-  private TopicService topicService;
+  @Autowired private TopicService topicService;
 
-  //Return list of topics
+  // Return list of topics
   @RequestMapping("/topics")
   public List<topicpojo> topic() {
 
     return topicService.getTopics();
   }
 
-  //Return base on the name (Single value)
+  // Return base on the name (Single value)
   @RequestMapping("/topics/{name}")
   public topicpojo getTopics(@PathVariable String name) {
     return topicService.getTopics(name);
   }
 
-  //add a post method
+  // add a post method
   @RequestMapping(method = RequestMethod.POST, value = "/topics")
-  public  void addtopic(@RequestBody topicpojo topic)
-  {
+  public void addtopic(@RequestBody topicpojo topic) {
     topicService.addtopic(topic);
   }
 
+  // update a particualar Resource
+  @RequestMapping(method = RequestMethod.PUT, value = "/topics/{name}")
+  public void updatetopic(@RequestBody topicpojo topic, @PathVariable String name) {
+    topicService.updatetopic(topic, name);
+  }
 }
