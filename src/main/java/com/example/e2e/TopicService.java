8ec30d14d388c5e4f52ef667f27538e9cb9f4a3b
venuGopal23456 <venuvg2010@gmail.com>
2019-12-24 14:56:52 -0700

    Get Request for single ID

--- src/main/java/com/example/e2e/TopicService.java
+++ src/main/java/com/example/e2e/TopicService.java
@@ -8,26 +8,20 @@ import java.util.List;
 @Service
 public class TopicService {
 
-  private List<topicpojo> topics =Arrays.asList(
-            new topicpojo("venu", 25, 100),
-        new topicpojo("gopal", 67, 250),
-        new topicpojo("sharma", 87, 90),
-        new topicpojo("mani", 56, 96),
-        new topicpojo("tilo", 56, 26));
+  private List<topicpojo> topics =
+      Arrays.asList(
+          new topicpojo("venu", 25, 100),
+          new topicpojo("gopal", 67, 250),
+          new topicpojo("sharma", 87, 90),
+          new topicpojo("mani", 56, 96),
+          new topicpojo("tilo", 56, 26));
 
+  public List<topicpojo> getTopics() {
+    return topics;
+  }
 
-   public  List<topicpojo> getTopics()
-   {
-       return topics;
-
-   }
-
-   ///Return only specific object based on name
-   public  topicpojo getTopics(String name)
-   {
-       return topics.stream().filter(t -> t.getName().equals(name)).findFirst().get();
-}
-
-
-
+  /// Return only specific object based on name
+  public topicpojo getTopics(String name) {
+    return topics.stream().filter(t -> t.getName().equals(name)).findFirst().get();
+  }
 }
--- src/main/java/com/example/e2e/topics.java
+++ src/main/java/com/example/e2e/topics.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 /*This is the simple controller class
@@ -14,19 +13,24 @@ import java.util.List;
 @RestController
 public class topics {
 
-  @Autowired
-  private TopicService topicService;
+  @Autowired private TopicService topicService;
 
-  //Return list of topics
+  // Return list of topics
   @RequestMapping("/topics")
   public List<topicpojo> topic() {
 
     return topicService.getTopics();
   }
 
-  //Return base on the name (Single value)
-  @RequestMapping("/topics/{name}")
+  // Return base on the name (Single value)
+  /*@RequestMapping("/topics/{name}")
   public topicpojo getTopics(@PathVariable String name) {
+    return topicService.getTopics(name);
+  }*/
+
+  // or
+  @RequestMapping("/topics/{first}")
+  public topicpojo getTopics(@PathVariable("first") String name) {
     return topicService.getTopics(name);
   }
 }
